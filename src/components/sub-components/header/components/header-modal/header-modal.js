775c27420eda017c824f023cda4117e92401ba8a
Tyagi <vaishali.tyagi@capgemini.com>
2023-05-18 15:35:33 +0530

    Css changes

--- src/components/sub-components/header/components/header-modal/header-modal.js
+++ src/components/sub-components/header/components/header-modal/header-modal.js
@@ -98,7 +98,7 @@ return (
         </button>
       </div>
 
-      <div className="modal-container">
+      <div className="modal-container headerModal">
         <div className="input-field-container">
           <FormField className="form-modal__content">
             <label
--- src/components/sub-components/navbar/edit-navbar.js
+++ src/components/sub-components/navbar/edit-navbar.js
@@ -336,7 +336,7 @@ function EditNav(props) {
                   onChange={handleChange}
                 >
                   <option value="">Select</option>
-                  <option value="Normal">Transparent</option>
+                  <option value="Normal">Light</option>
                   <option value="Dark">Dark</option>
                   <option value="cg1">Blue</option>
                   <option value="cg2">Purple</option>
