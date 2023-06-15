389f7864d0c85bfddb92d2f20e622966aba4346f
sherlock118 <anandudare914@gmail.com>
2023-06-15 15:08:49 +0530

    alert modified

--- src/components/sub-components/login-signup/login-main.js
+++ src/components/sub-components/login-signup/login-main.js
@@ -1,88 +1,94 @@
 import React, { useState } from 'react';
+import useModal from "../../sub-components/use-modal/use-modal";
 import { Link } from "react-router-dom";
+import ShowSuccess from './showsuccess';
 import "./login-main.scss";
 
 function LoginMain(props) {
- const[passWordvalue, setPassWordValue] = useState("");
- const[usernameValue, setUserNameValue] = useState("");
-const[confrimpass, setconfrimPass] = useState("");
-const[passwordError, setpassWordError] = useState({});
-const [passwordType, setPasswordType] = useState("password");
-const [confirmpasswordType, setConfirmPasswordType] = useState("password");
-console.log(props, "props in main");
+  const[passWordvalue, setPassWordValue] = useState("");
+  const[usernameValue, setUserNameValue] = useState("");
+  const[confrimpass, setconfrimPass] = useState("");
+  const[passwordError, setpassWordError] = useState({});
+  const [passwordType, setPasswordType] = useState("password");
+  const [confirmpasswordType, setConfirmPasswordType] = useState("password");
+  console.log(props, "props in main");
 
-let passError = {};
-const passwordCheck =()=>{
-  let flag= true;
-  var lowerCaseLetters = /[a-z]/g;
-   var upperCaseLetters = /[A-Z]/g;
-   var numbers = /[0-9]/g;
-   const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+  const { open: openShow, close: closeShow, ModalWrapper: ModalWrapperShowSuccess } = useModal();
+
+
+  let passError = {};
+  const passwordCheck =()=>{
+    let flag= true;
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g;
+    
+  if(!usernameValue){
+    passError.user = " Please enter the user Name";
+  flag= false;
+  }
+  if(!confrimpass){
+    passError.confirm = " Please enter the confirm password";
+  flag= false;
+  }
+  if(!passWordvalue){
+  console.log("password is empty");
+  passError.text=" Please enter the password";
+  flag= false;
+  }else{
+    // console.log("password  not is empty");
   
- if(!usernameValue){
-  passError.user = " Please enter the user Name";
-flag= false;
- }
- if(!confrimpass){
-  passError.confirm = " Please enter the confirm password";
-flag= false;
- }
- if(!passWordvalue){
-console.log("password is empty");
-passError.text=" Please enter the password";
-flag= false;
- }else{
-  // console.log("password  not is empty");
- 
-  if(props.loginVal.captialValue === "Yes"){
-    console.log(passWordvalue, confrimpass, "valueeee");
-    if(!upperCaseLetters.test(passWordvalue)){
-      console.log("password  cpaital matched");
-      passError.capital=" Password must contain atleast one capital alphabet";
-    flag= false;
+    if(props.loginVal.captialValue === "Yes"){
+      console.log(passWordvalue, confrimpass, "valueeee");
+      if(!upperCaseLetters.test(passWordvalue)){
+        console.log("password  cpaital matched");
+        passError.capital=" Password must contain atleast one capital alphabet";
+      flag= false;
+      }
+    }
+    if(props.loginVal.numericValue === "Yes"){
+      console.log(passWordvalue, confrimpass, "valueeee");
+      if(!numbers.test(passWordvalue)){
+        console.log("password numeric matched");
+        passError.number=" Password must contain atleast one numeric value ";
+      flag= false;
+      }
+    }
+    if(props.loginVal.specialValue === "Yes"){
+      console.log(passWordvalue, confrimpass, "valueeee");
+      if(!specialChars.test(passWordvalue)){
+        console.log("password special matched");
+        passError.special=" Password must contain atleast one special character ";
+      flag= false;
+      }
+    }
+    if(props.loginVal.minRange > passWordvalue.length || props.loginVal.maxRange < passWordvalue.length){
+      console.log("min range",props.loginVal.minRange,passWordvalue.length,props.loginVal.maxRange );
+      passError.range=`Password must contain minimum ${props.loginVal.minRange} and maximum ${props.loginVal.maxRange} characters`;
+      flag= false;
+    }
+    if(passWordvalue != confrimpass){
+      console.log(passWordvalue, confrimpass, "va;uess");
+      flag= false;
+      passError.confirm="Confirm password and password value should match";
     }
   }
-  if(props.loginVal.numericValue === "Yes"){
-    console.log(passWordvalue, confrimpass, "valueeee");
-    if(!numbers.test(passWordvalue)){
-      console.log("password numeric matched");
-      passError.number=" Password must contain atleast one numeric value ";
-    flag= false;
-    }
-  }
-  if(props.loginVal.specialValue === "Yes"){
-    console.log(passWordvalue, confrimpass, "valueeee");
-    if(!specialChars.test(passWordvalue)){
-      console.log("password special matched");
-      passError.special=" Password must contain atleast one special character ";
-    flag= false;
-    }
-  }
-  if(props.loginVal.minRange > passWordvalue.length || props.loginVal.maxRange > passWordvalue.length){
-    console.log("min range",props.loginVal.minRange,passWordvalue.length,props.loginVal.maxRange );
-    passError.range=`Password must contain minimum ${props.loginVal.minRange} and maximum ${props.loginVal.maxRange} characters`;
-    flag= false;
-  }
-  if(passWordvalue != confrimpass){
-    console.log(passWordvalue, confrimpass, "va;uess");
-    flag= false;
-    passError.confirm="Confirm password and password value should match";
-  }
-}
 
- setpassWordError(passError);
-//  console.log(flag ,passError, "flag valueee");
-     return flag;
-     
+  setpassWordError(passError);
+  //  console.log(flag ,passError, "flag valueee");
+      return flag;
+      
 
-};
+  };
 
  const onSubmit =(event)=>{
   event.preventDefault();
   const checkEditValid =   passwordCheck();
   console.log(checkEditValid, "valid value");
   if(checkEditValid){
-    alert("You have login successfully");
+    // alert("You have login successfully");
+    openShow();
   }
 
  }
@@ -219,6 +225,10 @@ const displayConfirmPassword = ()=> {
 
     </div>
   </div>
+
+    <ModalWrapperShowSuccess >
+      <ShowSuccess close={closeShow}/>
+    </ModalWrapperShowSuccess>
   </>
   );
 };
---
+++ src/components/sub-components/login-signup/showsuccess.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import closebIcon from "../../asset/images/cross-icon.png";
+import FocusTrap from "focus-trap-react";
+// import './alert.scss';
+import './login-main.scss'
+
+function ShowSuccess(props) {
+
+    const{ close } = props;
+
+    return(
+        <>
+        <FocusTrap focusTrapOptions={{ onDeactivate: close }}>
+            <div className="alert_wrapper">
+                <div className={`alert-content `}>
+                    <div className={`alert-header border-bottom`}>
+                        <p>{ ("Success Alert")}
+                        </p>
+                        <button className="close-button" aria-label="close alert popup" onClick={close}>
+                            { <img src={closebIcon}></img> }
+                        </button>
+                    </div>
+            
+                    <div className="alert-container">
+                        <div className='icon-section'>
+                            { <i class="fa fa-check-circle fa-2x success-icon" aria-hidden="true"></i> }                        
+                        </div>
+
+                        <div className='text-section'>
+                            <p>{ "User is Logged or signed in successfully !! "}</p>
+                        </div>
+                        
+                        <div className='alertbtn-section'>
+                            <button className={`btn-alert`} aria-label="okay button" onClick={close}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </FocusTrap>
+        </>
+    )
+}
+
+export default ShowSuccess;
